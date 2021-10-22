51a32b2320a5d57fe2f7a58b70e426e38bb3621a
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-10-23 01:11:22 +0600

    added header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Container, Nav, Navbar, Stack } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Mongo-Crud</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Stack direction="horizontal" gap={3}>
+                                <NavLink
+                                    className='text-light text-decoration-none'
+                                    to="/home"
+                                    activeStyle={{
+                                        fontWeight: "bold",
+                                        color: "red"
+                                    }}
+                                >
+                                    Home
+                                </NavLink>
+                                <NavLink
+                                    className='text-light text-decoration-none'
+                                    to="/addproduct"
+                                    activeStyle={{
+                                        fontWeight: "bold",
+                                        color: "red"
+                                    }}
+                                >
+                                    AddProduct
+                                </NavLink>
+                                <NavLink
+                                    className='text-light text-decoration-none'
+                                    to="/updateproduct"
+                                    activeStyle={{
+                                        fontWeight: "bold",
+                                        color: "red"
+                                    }}
+                                >
+                                    UpdateProduct
+                                </NavLink>
+                            </Stack>
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
