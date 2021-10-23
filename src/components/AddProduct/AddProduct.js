49f95c2c5094a173f038ad2290d6b5beb023da94
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-10-23 18:54:04 +0600

    update

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { Alert, FloatingLabel, Form } from 'react-bootstrap';
 
 const AddProduct = () => {
+    const [displayMessage, setDisplayMessage] = useState(false);
+    const nameRef = useRef();
+    const descRef = useRef();
+    const priceRef = useRef();
+
+    const handleAddProduct = () => {
+        setDisplayMessage(false);
+        const name = nameRef.current.value;
+        const desc = descRef.current.value;
+        const price = priceRef.current.value;
+
+        const newProduct = { name, desc, price };
+        fetch('http://localhost:5000/products', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(newProduct),
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+
+                    setDisplayMessage(true);
+
+
+                    // alert('product added successfully');
+
+                    //e.target.reset();
+                }
+
+            })
+    }
     return (
         <div>
-            <h2>AddProduct</h2>
+            <h2 className='text-center fw-bold my-5 fs-1'>Add Your Favorite Product</h2>
+            <div className='container'>
+                <div >
+                    {
+                        displayMessage && <Alert variant={'success'}>
+                            This is a alert—check it out!
+                        </Alert>
+                    }
+                    <FloatingLabel
+                        label="Name"
+                        className="mb-3"
+                    >
+                        <Form.Control type="text" ref={nameRef} placeholder="Name" />
+                    </FloatingLabel>
+                    <FloatingLabel type="text" ref={descRef} label="Description">
+                        <Form.Control
+                            as="textarea"
+                            placeholder="Leave a comment here"
+                            style={{ height: '100px' }}
+                        />
+                    </FloatingLabel>
+                    <FloatingLabel className='mt-3' label="Price">
+                        <Form.Control ref={priceRef} type="text" placeholder="Price" />
+                    </FloatingLabel>
+                    <button onClick={handleAddProduct} className='btn btn-danger mt-3' value="AddProduct" >Add P</button>
+                    {/* <div className="btn btn-danger mt-3">AddProduct</div> */}
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import { Button } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/products')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
+    //DELETE AN Products
+    const handleDeleteUser = id => {
+        const url = `http://localhost:5000/products/${id}`
+        fetch(url, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    alert('deleted successfully');
+                    const remainingProducts = products.filter(product => product._id !== id);
+                    setProducts(remainingProducts);
+                }
+            })
+    }
     return (
         <div>
-            <h2>Home</h2>
-        </div>
+            <h2 className='text-center my-5'>Products Vailable : {products.length}</h2>
+            <div className="row container">
+                {
+                    products.map(product =>
+
+                        < div className="col-lg-3 border border ms-5 my-3" >
+                            <div className="container">
+                                <h4>{product.name}</h4>
+                                <p>{product.name}</p>
+                                <h6>{product.price}</h6>
+                                <Button onClick={() => handleDeleteUser(product._id)} variant="danger">Delete</Button>
+                                <Button className='ms-5' variant="success">Update</Button>
+                            </div>
+
+                        </div>
+                    )
+                }
+            </div>
+
+        </div >
     );
 };
 
